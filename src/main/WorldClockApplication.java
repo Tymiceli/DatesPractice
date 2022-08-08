2e9de67b59a92c5d97cb013f9e8715d856b5ba5e
Tyler Miceli <tymiceli24@gmail.com>
2022-08-08 12:56:45 -0400

    Update WorldClockApplication.java
    Added more cities to the world clock.

--- src/main/WorldClockApplication.java
+++ src/main/WorldClockApplication.java
@@ -14,25 +14,34 @@ public class WorldClockApplication {
 	
 	private final static ZoneId BRISBANE = ZoneId.of("Australia/Brisbane");
 	private final static ZoneId DUBLIN = ZoneId.of("Europe/Dublin");
+	private final static ZoneId TORONTO = ZoneId.of("America/Toronto");
+	private final static ZoneId BEIJING = ZoneId.of("Asia/Shanghai");
+	private final static ZoneId ROME = ZoneId.of("Europe/Rome");
 
+	
 	public static void main(String[] args) {
 		
 
 		// first we need to capture what the time is at this exact moment...
 		ZonedDateTime zonedDateTime = ZonedDateTime.now();
-
+		System.out.println(zonedDateTime);
 		
+//		zonedDateTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM));
+		System.out.println("Toronto, Canada -> " + zonedDateTime.withZoneSameInstant(TORONTO).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM)));
+		System.out.println("Brisbane, Australia -> " +zonedDateTime.withZoneSameInstant(BRISBANE).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM)));
+		System.out.println("Dublin, Ireland -> " + zonedDateTime.withZoneSameInstant(DUBLIN).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM)));
+		System.out.println("Shanghai, China -> " + zonedDateTime.withZoneSameInstant(BEIJING).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM)));
+		System.out.println("Rome, Italy -> " + zonedDateTime.withZoneSameInstant(ROME).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a").ofLocalizedDateTime(FormatStyle.MEDIUM)));
 		// from here you will need to create three system outs to print out the times and dates
 		
 		// ZonedDateTime has a method called withZoneSameInstant(<ZoneId>) which gives you the exact date time in the
-		// time zone you put in using tbe zone id.  Try putting in zonedDateTime.withZoneSameInstant(BRISBANE)...
+		// time zone you put in using the zone id.  Try putting in zonedDateTime.withZoneSameInstant(BRISBANE)...
 		
-		// after we grabbed the time in our coutnry, we need to format the output using our DateTimeFormatter...
+		// after we grabbed the time in our country, we need to format the output using our DateTimeFormatter...
 		
 		// we need to format it in a way that is easily readable, for this assignment, use FormatStyle.MEDIUM
 		
 		// ... .format(DateTImeFormatter.ofLocalizedDateTIme(FormatStyle.MEDIUM));
-		
 		
 
 	}
